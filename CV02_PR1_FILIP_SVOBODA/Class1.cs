6b8a05e3121db0be956233071f64c77cc5a48be4
Filip Svoboda <st58611@upce.cz>
2021-03-03 14:48:32 +0100

    Přidejte soubory projektu.

---
+++ CV02_PR1_FILIP_SVOBODA/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace CV02_PR1_FILIP_SVOBODA
+{
+    public static class Reading
+    {
+        public static double ReadDouble(string text)
+        {
+            while (true)
+            {
+                Console.Write(text + ": ");
+                string value = Console.ReadLine();
+
+                double y;
+
+                if (double.TryParse(value, out y))
+                {
+                    return y;
+                }
+                else
+                {
+                    Console.WriteLine("Not a correct double value, please try again");
+                }
+            }
+        }
+
+        public static int ReadInt(string text)
+        {
+            while (true)
+            {
+                Console.Write(text + ": ");
+                string value = Console.ReadLine();
+
+                int y;
+
+                if (int.TryParse(value, out y))
+                {
+                    return y;
+                }
+                else
+                {
+                    Console.WriteLine("Not a correct int value, please try again");
+                }
+            }
+        }
+
+        public static char ReadChar(string text)
+        {
+            Console.Write(text + ": ");
+            string value = Console.ReadLine();
+
+            if (value.Length > 1)
+            {
+                throw new Exception("Console.ReadLine() returns string with more than one char, cannot parse.");
+            }
+
+            return value.ToCharArray()[0];
+        }
+
+        public static string ReadString(string text)
+        {
+            Console.Write(text + ": ");
+            return Console.ReadLine();
+        }
+    }
+}
